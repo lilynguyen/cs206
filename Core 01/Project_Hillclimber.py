86768285fbcb81021b48a574f719004df93c5415
Lily Nguyen <lilynguyen@ip0af54cf9.int.uvm.edu>
2016-02-02 22:45:14 -0500

    graph c

--- Core 01/Project_Hillclimber.py
+++ Core 01/Project_Hillclimber.py
@@ -13,10 +13,13 @@ def main():
 	# fits = hill_climber()
 	# plot_graph_a(fits)
 
-	for i in range(0,5):
-		fits = hill_climber()
-		plot_graph_b(fits)
-	plt.show()
+	# for i in range(0,5):
+	# 	fits = hill_climber()
+	# 	plot_graph_b(fits)
+	# plt.show()
+
+	genes = hill_climber()
+	plot_graph_c(genes)
 
 def matrix_create(rows, columns):
 	return np.zeros((rows, columns), dtype='f')
@@ -40,8 +43,27 @@ def matrix_perturb(parent, prob):
 				child[i][j] = random.random()
 	return child
 
+# def hill_climber():
+# 	fits = matrix_create(1,GENERATIONS)
+
+# 	parent = matrix_create(1,50)
+# 	parent = matrix_random(parent)
+# 	parentFitness = fitness(parent)
+
+# 	for currentGeneration in range(GENERATIONS):
+# 		child = matrix_perturb(parent, PROBABILITY)
+# 		childFitness = fitness(child)
+
+# 		if childFitness > parentFitness:
+# 			parent = child
+# 			parentFitness = childFitness
+
+# 		fits[0][currentGeneration] = parentFitness
+
+# 	return fits
+
 def hill_climber():
-	fits = matrix_create(1,GENERATIONS)
+	genes = matrix_create(50,GENERATIONS)
 
 	parent = matrix_create(1,50)
 	parent = matrix_random(parent)
@@ -55,9 +77,13 @@ def hill_climber():
 			parent = child
 			parentFitness = childFitness
 
-		fits[0][currentGeneration] = parentFitness
+		copiedParent = copy.deepcopy(parent)
+		copiedParent = np.transpose(copiedParent) # columns are now rows, orient like graph c
 
-	return fits
+		for geneNum in range(0,50):
+			genes[geneNum][currentGeneration] = copiedParent[geneNum][0]
+
+	return genes
 
 def plot_graph_a(fits):
 	plt.plot(fits[0])
@@ -69,5 +95,11 @@ def plot_graph_b(fits):
 	plt.plot(fits[0])
 	plt.xlabel('Generation')
 	plt.ylabel('Fitness')
+
+def plot_graph_c(genes):
+	plt.imshow(genes, cmap=cm.gray, aspect='auto', interpolation='nearest')
+	plt.xlabel('Generation')
+	plt.ylabel('Gene')
+	plt.show()
 
 main()
